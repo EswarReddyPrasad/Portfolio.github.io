4778c65add4c27d73d8eabcc4858557a791261a1
EswarReddyPrasad <166885973+EswarReddyPrasad@users.noreply.github.com>
2024-04-14 01:11:15 -0400

    Update compl.py

--- compl.py
+++ compl.py
@@ -58,7 +58,7 @@ def create_choropleth_map(company):
     return fig
 
 app = dash.Dash(__name__)
-
+server= app.server
 app.layout = html.Div([
     html.H1('Customer Disputes Dashboard'),
     html.Div([
